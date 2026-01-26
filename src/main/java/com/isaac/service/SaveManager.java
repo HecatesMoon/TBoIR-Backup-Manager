87db9b7e5814b338749000019e7b5b40fc9bb910
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-01-26 19:15:43 -0300

    implement backup logic for isaac save files

--- src/main/java/com/isaac/service/SaveManager.java
+++ src/main/java/com/isaac/service/SaveManager.java
@@ -1,18 +1,39 @@
 package com.isaac.service;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
 
 import com.isaac.Config;
 
 public class SaveManager {
     public static void backup(){
-        try (Stream<Path> saveFiles = Files.walk(Config.ORIGIN_PATH)) {
-            saveFiles.forEach(file -> System.err.println(file));
+        try {
+            
+            Files.createDirectories(Config.BACKUP_PATH);
+
+            try (Stream<Path> saveFiles = Files.walk(Config.ORIGIN_PATH, 1)) {
+                saveFiles.filter(file -> file.getFileName()
+                                             .toString()
+                                             .contains("persistentgamedata"))
+                                             .forEach(file -> copyFile(file, Config.BACKUP_PATH));
+            } catch (IOException e) {
+                System.err.println("Failed trying to access folder: " + e.getMessage());
+            }
         } catch (IOException e) {
-            System.out.println("Failed trying to access folder: " + e.getMessage());
+            System.err.println("Failed trying to create the backup folder: " + e.getMessage());
+        }
+    }
+
+    private static void copyFile(Path sourceFile, Path destinationPath){
+        try {
+            Path filePath = destinationPath.resolve(sourceFile.getFileName());
+            Files.copy(sourceFile, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.err.println("Failed trying to copy files: " + e.getMessage());
         }
     }
 }
