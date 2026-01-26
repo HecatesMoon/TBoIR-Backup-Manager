3a16ef190ce341281a6d6e9a0354e3e5f12fc48e
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-01-26 19:17:06 -0300

    change backup folders name

--- src/main/java/com/isaac/Config.java
+++ src/main/java/com/isaac/Config.java
@@ -8,5 +8,5 @@ public class Config {
     private static final String BASE_DIR = System.getProperty("user.dir");
 
     public static final Path ORIGIN_PATH = Path.of(USER_HOME, "Documents", "My Games", "Binding of Isaac Repentance+");
-    public static final Path BACKUP_PATH = Path.of(BASE_DIR, "Backups");
+    public static final Path BACKUP_PATH = Path.of(BASE_DIR, "Isaac-Backups");
 }
