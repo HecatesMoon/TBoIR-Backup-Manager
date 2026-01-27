8e2356904cdd2625694c799c7a4b6ff0ec9e75f9
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-01-27 19:57:58 -0300

    feat: implemented dynamic paths with configuration file

--- src/main/java/com/isaac/Config.java
+++ src/main/java/com/isaac/Config.java
@@ -1,12 +1,54 @@
 package com.isaac;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Properties;
 
 public class Config {
+
+    private static Properties configs = new Properties();
+    private static final String CONFIG_FILE = "config.properties";
+
     //todo: add os compatibilty
     private static final String USER_HOME = System.getProperty("user.home");
     private static final String BASE_DIR = System.getProperty("user.dir");
 
-    public static final Path ORIGIN_PATH = Path.of(USER_HOME, "Documents", "My Games", "Binding of Isaac Repentance+");
-    public static final Path BACKUP_PATH = Path.of(BASE_DIR, "Isaac-Backups");
+    private static final Path DEFAULT_ORIGIN_PATH = Path.of(USER_HOME, "Documents", "My Games", "Binding of Isaac Repentance+");
+    private static final Path DEFAULT_BACKUP_PATH = Path.of(BASE_DIR, "Isaac-Backups");
+
+    public static Path ORIGIN_PATH;
+    public static Path BACKUP_PATH;
+
+    static{
+
+        if (!Files.exists(Path.of(CONFIG_FILE))){
+            configs.setProperty("ORIGIN_PATH", DEFAULT_ORIGIN_PATH.toString());
+            configs.setProperty("BACKUP_PATH", DEFAULT_BACKUP_PATH.toString());
+            storeProperties();
+            loadProperties();
+        } else {
+            loadProperties();
+        }
+
+        ORIGIN_PATH = Path.of(configs.getProperty("ORIGIN_PATH", DEFAULT_ORIGIN_PATH.toString()));
+        BACKUP_PATH = Path.of(configs.getProperty("BACKUP_PATH", DEFAULT_BACKUP_PATH.toString()));
+    }
+
+    private static void loadProperties (){
+        try (FileInputStream fis = new FileInputStream(CONFIG_FILE)){
+            configs.load(fis);
+        } catch (IOException e){
+            System.err.println("Failed trying to read config.properties: " + e.getMessage());
+        }
+    }
+    private static void storeProperties(){
+        try (FileOutputStream fos = new FileOutputStream(CONFIG_FILE)) {
+            configs.store(fos, "Default path configs for Isaac Backup");
+        } catch (IOException e) {
+            System.err.println("Failed trying to store config.properties: " + e.getMessage());
+        }
+    }
 }
