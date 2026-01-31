b7184c35dca20e68e548330f1d27a10b0aa76860
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-01-31 17:53:26 -0300

    feat: add a submenu in the cli that allows you to change paths from the console

--- src/main/java/com/isaac/ui/CLI.java
+++ src/main/java/com/isaac/ui/CLI.java
@@ -8,47 +8,61 @@ import com.isaac.service.SaveManager;
 
 public class CLI {
 
-    private Boolean isRunning;
+    private Boolean cliLoopRuns;
     private Scanner scanner;
+    
+    private Boolean pathChangeLoop;
 
     public CLI (){
-        this.isRunning = true;
+        this.cliLoopRuns = true;
         this.scanner = new Scanner(System.in);
     }
 
     public void cliLoop(){
         
         System.out.println("Welcome to TBoIR Backup Maker");
-        printMenu();
+        printMenu("main");
         
-        while (this.isRunning) {
+        while (this.cliLoopRuns) {
             
             System.out.print("> ");
-            menuOption();
+            menuOption(this.scanner.nextLine());
             
         }
         scanner.close();
     };
 
-    private void printMenu(){
-        System.out.println("----------MENU----------");
-        System.out.println("1. Show current configured paths");
-        System.out.println("2. Backup the savefiles (from source to backup folder)");
-        System.out.println("3. Restore the savefiles (from backup to source folder)");
-        System.out.println("4. Exit");
-        System.out.println("What do you want to do? (press the number and then enter)");
+    private void printMenu(String whichMenu){
+        switch (whichMenu) {
+            case "main":
+                System.out.println("----------MENU----------");
+                System.out.println("1. Show current configured paths");
+                System.out.println("2. Backup the savefiles (from source to backup folder)");
+                System.out.println("3. Restore the savefiles (from backup to source folder)");
+                System.out.println("4. Change Paths");
+                System.out.println("5. Exit");
+                System.out.println("What do you want to do? (press the number and then enter)");
+                break;
+            case "paths":
+                System.out.println("----------MENU----------");
+                System.out.println("1. Change Origin Path");
+                System.out.println("2. Change Backup Path");
+                System.out.println("3. Exit Path Configuration");
+                break;
+            default:
+                System.out.println("Write a valid number");
+                break;
+        }
     }
 
-    private void menuOption(){
-        String choosenOption;
-        choosenOption = this.scanner.nextLine();
+    private void menuOption(String choosenOption){
 
         switch (choosenOption) {
-            case "1":
+            case "1": //prints actual paths
                 System.out.println("Source Path: " + Config.getOriginPath().toString());
                 System.out.println("Backup Path: " + Config.getBackupPath().toString());
                 break;
-            case "2":
+            case "2": // backups data
                 if (SaveManager.backup()){
                     System.out.println("Backup Done!");
                 } else {
@@ -56,20 +70,72 @@ public class CLI {
                 }
                 
                 break;
-            case "3":
+            case "3": // restores backup
                 if (RestoreManager.restore()){
                     System.out.println("Savefiles restored from backup!");
                 } else {
                     System.err.println("Restoring failed");
                 }
                 break;
-            case "4":
+            case "4": // opens path configuration
+                changePathLoop();
+                break;
+            case "5": // exits program
                 System.out.println("Closing program");
-                isRunning = false;
+                cliLoopRuns = false;
                 break;
             default:
                 System.out.println("Write a valid number");
                 break;
         }
     }
+
+    private void changePathLoop(){
+
+        pathChangeLoop = true;
+
+        System.out.println("------CHANGE PATHS------");
+        System.out.println("Origin Path:" + Config.getOriginPath());
+        System.out.println("Backup Path:" + Config.getBackupPath());
+        printMenu("paths");
+
+        while(pathChangeLoop){
+            System.out.print("> ");
+            changePath(this.scanner.nextLine());
+        }
+    }
+
+    private void changePath(String choosenOption){
+        
+        switch (choosenOption) {
+            case "1":// changes origin path
+                System.out.println("Input the path you want to use for origin");
+                System.out.print(">");
+                if (Config.setOriginPath(this.scanner.nextLine())){
+                    System.out.println("The origin path was changed succesfully");
+                    System.out.println(Config.getOriginPath());
+                } else {
+                    System.out.println("The path couldn't be changed, the path does not exist or is not writeable");
+                }
+                break;
+            case "2":// changes backup path
+                System.out.println("Input the path you want to use for backup");
+                System.out.print(">");
+                if (Config.setBackupPath(this.scanner.nextLine())){
+                    System.out.println("The backup path was changed succesfully");
+                    System.out.println(Config.getBackupPath());
+                } else {
+                    System.out.println("The path couldn't be changed, the path does not exist or is not writeable");
+                }
+                break;
+            case "3"://closes submenu
+                System.out.println("Going back to main menu");
+                pathChangeLoop = false;
+                break;
+            default:
+                System.out.println("Write a valid number");
+                break;
+        }
+    }
+
 }
