9bcb4edd27d6469887f956d8d34a47a577ab61f1
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-02-03 20:18:48 -0300

    feat: implement GameVersion enum for optimal and consisten folder and path writing

---
+++ src/main/java/com/isaac/service/GameVersion.java
@@ -0,0 +1,36 @@
+package com.isaac.service;
+
+import java.nio.file.Path;
+
+import com.isaac.Config;
+
+public enum GameVersion {
+    REBIRTH("Binding of Isaac Rebirth", "binding of isaac rebirth", "Binding of Isaac Rebirth"),
+    AFTERBIRTH("Binding of Isaac Afterbirth", "binding of isaac afterbirth", "Binding of Isaac Afterbirth"),
+    AFTERBIRTH_PLUS("Binding of Isaac Afterbirth+", "binding of isaac afterbirth+", "Binding of Isaac Afterbirth+"),
+    REPENTANCE("Binding of Isaac Repentance", "Binding of Isaac Repentance", "not available"),
+    REPENTANCE_PLUS("Binding of Isaac Repentance+", "Binding of Isaac Repentance+", "not available");
+
+    private final String winFolder;
+    private final String linuxFolder;
+    private final String macFolder;
+
+    GameVersion (String winFolder, String linuxFolder, String macFolder){
+        this.winFolder = winFolder;
+        this.linuxFolder = linuxFolder;
+        this.macFolder = macFolder;
+    }
+
+    public Path getFolderName (){
+        if (Config.isWindows){
+            return Path.of(winFolder);
+        } else if (Config.isLinux) {
+            return Path.of(linuxFolder);
+        } else if (Config.isMac) {
+            return Path.of(macFolder);
+        }
+
+        throw new UnsupportedOperationException("Operative System not detected, or not supported.");
+    } 
+}
+    
